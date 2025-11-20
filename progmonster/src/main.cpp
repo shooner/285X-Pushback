c0dbba8f158322fe07b4991a2649f8243fadc342
S1826356 <bubbles79123@gmail.com>
2025-11-20 03:56:16 -0600

    merg skills and auton
    popipopipopopipo

--- progmonster/src/main.cpp
+++ progmonster/src/main.cpp
@@ -579,7 +579,6 @@ void autonomous() {
     pros::delay(2000);
     convState(0,-1); //stop motors
 
-
     //parking
     chassis.turnToPoint(a*59.6, b*-28.6, 500);
     pros::delay(700);
@@ -590,6 +589,7 @@ void autonomous() {
     chassis.moveToPoint(a*60.7, b*-1, 800);
     pros::delay(700);
 
+    
     // End of autonomous routine: stop conveyor task and mark auton finished
     autonRunning = false;
     if (convTaskPtr) {
@@ -603,6 +603,34 @@ void autonomous() {
     onetwo_motor.move(0);
     threefour_motor.move(0);
     five_motor.move(0);
+
+/*
+    // Auton maxxing auton auton not skills autonomous
+    chassis.setPose(a*50, b*17, 180);
+    chassis.turnToPoint(a*50, b*47, 500);
+    chassis.moveToPoint(a*50, b*47, 1000);
+
+    chassis.turnToPoint(a*56.5, b*47, 500);
+    scraper.set_value(true);
+    chassis.moveToPoint(a*56.5, b*47, 700);
+    convState(0, 0); //intake 3 red 3 blue
+    pros::delay(2700);
+    convState(0, -1); //stop motors
+    chassis.moveToPoint(a*56.5, b*47, 500, {.forwards=false});
+
+    chassis.turnToPoint(a*31.3, b*11.8, 700); //trying to intake the corner stack
+    convState(0,0); //intake the 3 red corner blocks
+    chassis.moveToPoint(a*31.3, b*11.8, 1000);
+
+    chassis.turnToPoint(a*23, b*22.7, 1000);
+    chassis.moveToPoint(a*23, b*22.7, 700);
+
+    chassis.turnToPoint(a*13.7, b*13.5, 1000);
+    chassis.moveToPoint(a*13.7, b*13.5, 700);
+
+    pros::delay(2000);
+    convState(1,1); //outtake center lower from bottom basket which has red
+*/
 
     // hold here (typical auton ends and does not return)
     while (true) {
